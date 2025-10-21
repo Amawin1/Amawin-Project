5d6e9adb5a2179b9146a89dd870ee2d5ac6c37c6
Amawin1 <hostingk906@gmail.com>
2025-10-21 18:40:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// Hamburger Menu Toggle
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector("nav ul");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
-
-// Set current year in
+function toggleMenu() {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('active');
+}
