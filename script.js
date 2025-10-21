519c13c5ed35da1c7c1e19a27f68de73b5e517e9
Amawin1 <hostingk906@gmail.com>
2025-10-21 13:03:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ document.getElementById('year').textContent = new Date().getFullYear();
 
 // script.js
 document.getElementById('year').textContent = new Date().getFullYear();
-
-document.querySelector('.more-button').addEventListener('click', function() {
-  this.classList.toggle('active');
-});
