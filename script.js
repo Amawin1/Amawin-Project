43919e972a573ce09740271c603ff20f6557d74e
Amawin1 <hostingk906@gmail.com>
2025-10-21 12:31:58 +0530

    update mobile 1,1

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ document.getElementById('year').textContent = new Date().getFullYear();
 // script.js
 document.getElementById('year').textContent = new Date().getFullYear();
 
+document.querySelector('.more-button').addEventListener('click', function() {
+  this.classList.toggle('active');
+});
